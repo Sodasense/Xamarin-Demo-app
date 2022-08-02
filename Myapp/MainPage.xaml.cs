30c8e67cd782b2319a1fd7e36b4c89a54033c21a
ThanosVk <66605182+ThanosVk@users.noreply.github.com>
2022-08-02 18:34:50 +0300

    Add files via upload

---
+++ Myapp/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Myapp
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+		int cnt = 0;
+		String msg = "";
+		public async void DisplayAlert(object obj,EventArgs eventArgs)
+        {
+			cnt++;
+            if (cnt == 1)
+            {
+				msg = "You pressed the button " + cnt + " time!";
+            }
+            else
+            {
+				msg = "You pressed the button " + cnt + " times!";
+			}
+			await DisplayAlert("Alert", msg, "Ok");
+        }
+	}
+}
